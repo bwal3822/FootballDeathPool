650590ed84722373a5a1008500b7c5281312dced
bwal3822 <bwaldo1@gmail.com>
2023-09-18 12:21:58 -0500

    commit on Main

--- DeathPool.py
+++ DeathPool.py
@@ -1,1 +1,15 @@
 # Football Death Pool
+
+#Enter Player Names
+
+#Enter Player Picks
+
+#Enter Game Results
+
+#Player Eliminated
+
+#Player Wins
+
+#Show Picks of all players for current week
+
+#Show Picks of all players for all weeks
