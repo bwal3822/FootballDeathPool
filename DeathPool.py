888703591772a07fa01c35ff495dcaedd5b4ef55
bwal3822 <bwaldo1@gmail.com>
2023-09-18 15:32:31 -0500

    Initialize CSV

--- DeathPool.py
+++ DeathPool.py
@@ -1,4 +1,24 @@
 # Football Death Pool
+import csv
+import os
+
+# Initialize file path
+csv_file_path = '/mnt/data/FootballDeathPool.csv'
+
+def initialize_pool(weeks):
+    # Create CSV file and write headers
+    if not os.path.exists(csv_file_path):
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            headers = ['Player', 'Status'] + [f"Week {i+1}" for i in range(weeks)]
+            csvwriter.writerow(headers)
+
+# Assuming an 18-week NFL season
+initialize_pool(18)
+
+# Show the first few lines of the initialized CSV to confirm its structure
+pd.read_csv(csv_file_path).head()
+
 
 #Enter Player Names
 
