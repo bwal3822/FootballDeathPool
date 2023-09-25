4f2518592f51ae4ad2452f9b8d199304aa508ba8
bwal3822 <bwaldo1@gmail.com>
2023-09-25 15:46:21 -0500

    Update future weeks to Eliminated

--- DeathPool.py
+++ DeathPool.py
@@ -161,7 +161,9 @@ def check_elimination(player_list_df, weekly_game_outcome_df, current_week):
                     print(f"Player {row['Player']} is eliminated.")
             else:
                 print(f"Player {row['Player']} is already eliminated. Skipping.")
-                
+                for future_week in range(week, current_week + 1): #Update future weeks to Eliminated
+                    player_list_df.at[index, f"Week{future_week}"] = 'Eliminated'
+    
     player_list_df.to_csv(csv_file_path, index=False)
     return player_list_df
 
